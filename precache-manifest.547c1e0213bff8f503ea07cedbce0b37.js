891673315cb799db42c63fb19326e7b3191888d1
unknown <you@example.com>
2020-07-23 03:25:41 -0300

    Updates

---
+++ precache-manifest.547c1e0213bff8f503ea07cedbce0b37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf76acb87c18c1e568aaddb52a9bb77c",
+    "url": "/wesleymtx/teste/index.html"
+  },
+  {
+    "revision": "4bfde1c20d5a85630807",
+    "url": "/wesleymtx/teste/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6638f6ad5da086c69ffa",
+    "url": "/wesleymtx/teste/static/js/2.69de081f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wesleymtx/teste/static/js/2.69de081f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bfde1c20d5a85630807",
+    "url": "/wesleymtx/teste/static/js/main.7150d3e0.chunk.js"
+  },
+  {
+    "revision": "ef6ba556ab5afc536f83",
+    "url": "/wesleymtx/teste/static/js/runtime-main.932afe1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wesleymtx/teste/static/media/logo.5d5d9eef.svg"
+  }
+]);
